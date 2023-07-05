d11f24a271902f08e07a7fc8f70007969a4b0712
Prince-of-sea@KMR <94970975+Prince-of-sea@users.noreply.github.com>
2023-07-05 20:38:23 +0900

    [Fix]Teddit Please on Greasemonkey

--- Teddit_on_gm/Teddit_on_gm.user.js
+++ Teddit_on_gm/Teddit_on_gm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Teddit Please on Greasemonkey
 // @namespace       https://github.com/Prince-of-sea/UserscriptWorks_mk2/
-// @version         0.1.0
+// @version         0.1.1
 // @description:ja  Teddit Please拡張機能のUserscript版です
 // @author          Prince-of-sea
 // @downloadURL     https://raw.githubusercontent.com/Prince-of-sea/UserscriptWorks_mk2/main/Teddit_on_gm/Teddit_on_gm.user.js
@@ -14,7 +14,7 @@
 
 (function () {
 	let t1 = 'https://www.reddit.com'
-	let redirect_url = 'https://teddit.zaggy.nl/' //ここのURLの編集でインスタンス変更可能
+	let redirect_url = 'https://teddit.zaggy.nl' //ここのURLの編集でインスタンス変更可能
 	
 	let jump_url = window.location.href.replace(t1, redirect_url)
 	
